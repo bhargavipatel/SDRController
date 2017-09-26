dd3099eacb8d9cc9cd19d2fe2ac672f0e8b7a1d6
Bhargavi <bhargavi@umd.edu>
2017-09-26 17:40:03 -0400

    final test error

--- app/pidcont.cpp
+++ app/pidcont.cpp
@@ -11,8 +11,13 @@ pidcont::pidcont() {
 pidcont::~pidcont() {
 }
 double pidcont::computenew_velo(double targetvelo, double actuvelo, double Kp_input, double Ki_input, double Kd_input) {
-  double someconst = 3.1;
-  return someconst;
+  //double someconst = 3.1;
+  //return someconst;
+  targetvelo = 10;
+  actuvelo = 2;
+  double error1 = targetvelo-actuvelo;
+  return(Kp_input*error1 + Kd_input*error1 + Ki_input*error1);
+  //-2.325
 }
 
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,34 @@
+/**
+ * @breif: Main
+ * @author Bhargavi
+ * @version 1.0
+ */
 #include <gtest/gtest.h>
+#include "../include/pidcont.hpp"
+// #include "../include/pidcont_test.hpp"
+// #include "../app/pidcont.cpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+/*
+ * @brief 
+ */
+//Kp = 0.5;Ki = 0.05;Kd = 1;perr = 0;err = 0;
+//targetvelo = 10
+//actualvelo = 2
+//computenew_velo(double targetvelo, double actuvelo, double Kp_input, double Ki_input, double Kd_input) {
+
+TEST(firsttest, unity) {
+  EXPECT_EQ(1,1);
+}
+
+/*
+ * @brief Test if the integral element is behaving correctly
+ */
+TEST(secondtest, unitTest2) {
+  pidcont a;
+  float temp = a.computenew_velo(10, 2, 0.5, 0.05, 1);	
+  EXPECT_FLOAT_EQ(temp, 12.4);
+  //EXPECT_EQ(1,1);
+}  
+TEST(thirdtest,unittest3) {
+    EXPECT_EQ(1,1);
 }
