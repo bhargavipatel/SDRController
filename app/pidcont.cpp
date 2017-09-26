a1c8104c259bc5b534b8e2d0cd5a7979ea587642
Bhargavi <bhargavi@umd.edu>
2017-09-25 23:40:36 -0400

    cmakelis

--- app/pidcont.cpp
+++ app/pidcont.cpp
@@ -5,15 +5,10 @@
  */
 
 #include <pidcont.hpp>
-
-
 pidcont::pidcont(){
-
 }
 pidcont::~pidcont(){
-
 }
-
 double pidcont::computenew_velo(double targetvelo, double actuvelo, double Kp_input, double Ki_input, double Kd_input) {
   double someconst = 3.1;
   return someconst;  
